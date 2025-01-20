2ce28fb77d43d5cbc167e80d66bbdbf6d880da74
BenFormantes <elian.b.formantes@vanderbilt.edu>
2025-01-20 02:55:30 -0800

    Adjust paths

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/stackline-oa",
+    base: "/stackline-oa/",
     plugins: [react()],
     server: {
         open: true,
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/stackline-oa",
+  base: "/stackline-oa/",
   plugins: [react()],
   server: {
     open: true,
