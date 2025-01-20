4038596b1203238dd00f0fc215d739c5b2e4da6b
BenFormantes <elian.b.formantes@vanderbilt.edu>
2025-01-20 03:03:56 -0800

    Fix api stub

--- src/app/services/retailItemsService.ts
+++ src/app/services/retailItemsService.ts
@@ -1,16 +1,18 @@
 import { AppThunk } from '../store/store'
 import { setError, setLoading, setRetailItem } from '../store/retailItemSlice'
+import payload from '../assets/stackline_frontend_assessment_data_2021.json';
 
-const baseUrl = 'http://localhost:3000/stackline-endpoint'
+//const baseUrl = 'http://localhost:3000/stackline-endpoint'
 export const fetchData = (): AppThunk => {
   return async (dispatch) => {
     dispatch(setLoading(true))
     try {
-      const response = await fetch(baseUrl)
-      if (!response.ok) {
-        throw new Error('Failed to fetch')
-      }
-      const data = await response.json()
+      //const response = await fetch(baseUrl)
+      //if (!response.ok) {
+      //  throw new Error('Failed to fetch')
+      //}
+      //const data = await response.json()
+      const data = payload[0]
       dispatch(setRetailItem(data))
       dispatch(setError(null))
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
